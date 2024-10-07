0fe747d34b8f1dbbaf69ec01cbcfa5779f9d6ecf
Ильдар <162967672+Kweit1989@users.noreply.github.com>
2024-10-07 13:49:53 +0300

    Delete work directory

--- work/Alpha marketing/js/main.js
+++
@@ -1,9 +0,0 @@
-import {menuButton, scroll, menuList} from './modules/menu.js';
-import {map} from './modules/map.js';
-import {modals} from './modules/modals.js'
-
-menuButton();
-scroll();
-menuList();
-map();
-modals();
